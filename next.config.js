e7e1d575acb93d9aae454a348fcb091c277b2052
LLLOCKED <vladmatvienko4@gmail.com>
2022-09-29 13:04:09 +0300

    update gitignore file

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-const path = require('path')
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname,'src/styles')],
+    includePaths: [path.join(__dirname, 'src/styles')]
   },
   images: {
-    domains: ['upload.wikimedia.org'],
+    domains: ['upload.wikimedia.org']
   },
   env: {
-    MAP_KEY: "AIzaSyAt6eDjDhxS2LxWoY_eu2k-RhobPYTuDO0"
+    MAP_KEY: 'AIzaSyAt6eDjDhxS2LxWoY_eu2k-RhobPYTuDO0'
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
